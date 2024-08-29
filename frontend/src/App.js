01549f02c52d15aea5f323c55c75001c8b1b8e21
Mustafa Naji <mustafanaji000@gmail.com>
2024-08-29 20:56:15 +0200

    Install tailwind and started a frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
