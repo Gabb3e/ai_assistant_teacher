af7d32732d43e484392d39a5c0230b2ca331f0b0
Mustafa Naji <mustafanaji000@gmail.com>
2024-09-02 15:42:37 +0200

    Added a topic selection page where the user can choose what topic the users want to be questioned on

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Onb2 from './Onb2';
 import Onb3 from './Onb3';
 import Onb4 from './Onb4';
 import Dashboard from './Dashboard';
+import TopicSelection from './TopicSelection';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/onb2" element={<Onb2 />} />
         <Route path="/onb3" element={<Onb3 />} />
         <Route path="/onb4" element={<Onb4 />} />
+        <Route path="/TopicSelection" element={<TopicSelection />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
