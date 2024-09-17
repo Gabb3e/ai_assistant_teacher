5cdca5d00c60d524065fe14ff6d45e272ccb48b2
Gabb3e <gabrielchandia@hotmail.com>
2024-09-17 15:30:13 +0200

    added footer and changed layout for landingpage

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="bg-gray-900 h-20">
+        <footer className="bg-gray-900 h-28">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-white sm:text-center">© 2024 <a href="" className="hover:underline">Logo™</a>. All Rights Reserved.
+                <span className="text-xl text-white sm:text-center">© 2024 <a href="" className="hover:underline">Logo™</a>. All Rights Reserved.
                 </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
+                <ul className="flex flex-wrap items-center mt-3 text-xl font-medium text-white sm:mt-0">
                     <li>
                         <a href="#" className="hover:underline me-4 md:me-6">About</a>
                     </li>
